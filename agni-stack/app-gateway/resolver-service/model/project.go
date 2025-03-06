3ef2901d23c455339415bccd1e96588b05ff235b
dipghoshraj <dipghoshraj@gmail.com>
2025-03-07 00:44:01 +0530

    AGNI-001: feat : added the model for app and prejcts

---
+++ agni-stack/app-gateway/resolver-service/model/project.go
@@ -0,0 +1,15 @@
+package model
+
+import (
+	"gorm.io/gorm"
+)
+
+type Project struct {
+	gorm.Model
+	ID          int64  `gorm:"primaryKey;autoIncrement" json:"id"`
+	Name        string `gorm:"size:255;not null" json:"name"`
+	Description string `gorm:"size:255" json:"description"`
+	OwnerID     int64  `json:"owner_id"`
+	Owner       User   `json:"owner"`
+	Apps        []App  `json:"apps"`
+}
