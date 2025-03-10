382d8f24e3a5f325efe2f4c28cbbe07f203415ab
dipghoshraj <dipghoshraj@gmail.com>
2025-03-11 00:10:47 +0530

    added the get prjects

--- agni-stack/app-gateway/repository/project.repository.go
+++ agni-stack/app-gateway/repository/project.repository.go
@@ -9,6 +9,8 @@ import (
 )
 
 type ProjectRepo interface {
+	CreateProject(ctx context.Context, input graphmodel.ProjectInput) (graphmodel.Project, error)
+	GetProjects(ctx context.Context) ([]graphmodel.Project, error)
 }
 
 func (pr *projRepo) CreateProject(ctx context.Context, input graphmodel.ProjectInput) (graphmodel.Project, error) {
@@ -33,4 +35,24 @@ func (pr *projRepo) CreateProject(ctx context.Context, input graphmodel.ProjectI
 	}
 
 	return graphproj, nil
+}
+
+func (pr *projRepo) GetProjects(ctx context.Context) ([]graphmodel.Project, error) {
+	var projectList []graphmodel.Project
+	var projects []dbmodel.Project
+
+	userID, ok := ctx.Value("user_id").(float64)
+	if !ok || userID == 0 {
+		return projectList, fmt.Errorf("missing user ID")
+	}
+
+	if err := database.DB.Where("projects.owner_id = ?", userID).Find(&projects).Error; err != nil {
+		return projectList, err
+	}
+
+	if err := database.DB.Model(&projects).Scan(&projectList).Error; err != nil {
+		return projectList, err
+	}
+
+	return projectList, nil
 }
